310c0f5b8d55d2adeec684ac7997a2f8eb7e8552
aaron-might <aaronmighty7@gmail.com>
2021-09-03 05:55:27 -0400

    writing the left arrow conditional

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const slides = document.querySelectorAll('.slide');
 const left = document.getElementById('left');
+carousel = document.getElementById('carousel')
 const right = document.getElementById('right');
 
-const SLIDE_COUNT = slides.length;
+const SLIDES_COUNT = slides.length;
+let current_slide = 0;
 left.addEventListener('click', () =>{
-
+current_slide++;
+if(current_slide > SLIDES_COUNT-1){
+    current_slide = 0;
+}
 });
 
 right.addEventListener('click', ()=>{
-    
+
 })
