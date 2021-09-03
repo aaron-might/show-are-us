b585397a5fba9c58fb74d291b1a05a114948ec49
aaron-might <aaronmighty7@gmail.com>
2021-09-03 05:50:51 -0400

    stating variables slide, left, right

---
+++ index.js
@@ -0,0 +1,12 @@
+const slides = document.querySelectorAll('.slide');
+const left = document.getElementById('left');
+const right = document.getElementById('right');
+
+const SLIDE_COUNT = slides.length;
+left.addEventListener('click', () =>{
+
+});
+
+right.addEventListener('click', ()=>{
+    
+})
