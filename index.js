ce8730b1132fdecd5b5ea2e8b5571e047dc7976f
aaron-might <aaronmighty7@gmail.com>
2021-09-04 02:43:40 -0400

    update background color with the each slide

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const slides = document.querySelectorAll('.slide');
 const left = document.getElementById('left');
-carousel = document.getElementById('carousel')
+carousel = document.getElementById('carousel');
 const right = document.getElementById('right');
 
 const SLIDES_COUNT = slides.length;
 let current_slide = 0;
 left.addEventListener('click', () => {
-    current_slide++;
-    if (current_slide > SLIDES_COUNT - 1) {
-        current_slide = 0;
-    }
-});
-
-right.addEventListener('click', () => {
     current_slide--;
     if (current_slide < 0 ) {
         current_slide = SLIDES_COUNT - 1;
     }
+    updateCarousel();
+});
+
+right.addEventListener('click', () => {
+    current_slide++;
+    if (current_slide > SLIDES_COUNT - 1) {
+        current_slide = 0;
+    }
+    updateCarousel();
 })
+
+function updateCarousel() {
+    carousel.style.transform = `translateX(${-current_slide * slides[0]. offsetWidth}px)`;
+    document.body.style.background = `#${slides[current_slide].getAttribute(
+            'data-bg'
+        )}`;
+            console.log(slides[current_slide].getAttribute('data-bg'));
+}
